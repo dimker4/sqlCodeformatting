d16bb4d62908de6b0b5f0b0841df5cfac98c6c33
dimker <dimker4@gmail.com>
2020-02-08 20:13:03 +0500

    simple query

---
+++ sqlFormating/sqlFormating/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sqlFormating
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var sqlText = richTextBox1.Text;
+
+            var f = new Formatting(sqlText);
+
+            richTextBox2.Text = f.makeFormated();
+        }
+    }
+
+    public class Formatting
+    {
+        public string text { get; }
+
+        public Formatting()
+        {
+            text = "empty";
+        }
+
+        public Formatting(string sqlText)
+        {
+            text = sqlText;
+        }
+
+        public string makeFormated()
+        {
+            var tmpText = "";
+            var newLine = "";
+            using (StringReader reader = new StringReader(text))
+            {
+                string line;
+                // Работаем по одной строке
+                while ((line = reader.ReadLine()) != null)
+                {
+                    newLine = line;
+                    var selectPos = line.IndexOf("select", StringComparison.CurrentCultureIgnoreCase);
+                    var fromPos = line.IndexOf("from", StringComparison.CurrentCultureIgnoreCase);
+                    var wherePos = line.IndexOf("where", StringComparison.CurrentCultureIgnoreCase);
+
+                    // проверяем, что бы SELECT и FROM были в одной строке
+                    if (selectPos != -1 && fromPos != -1)
+                    {
+                        // Если зашли, значит в одной строке, добавляем переход на новую
+                        newLine = line.Insert(fromPos, "\n");
+
+                        // Если WHERE в этой же строке, то тоже двигаем
+                        if (wherePos != -1)
+                        {
+                            newLine = newLine.Remove(wherePos, 1);
+                            newLine = newLine.Insert(wherePos, "\n");
+                            
+                        }
+                    }
+
+                }
+            }
+
+            return newLine.ToString();
+
+        }
+    }
+}
---
+++ sqlFormating/sqlFormating/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("sqlFormating")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sqlFormating")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("82b36612-6150-4aea-9b07-e195470082b3")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
